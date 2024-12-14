1e3b67c9deefd91a379d2fa0b24bbd5c61ecea5f
Prestgg1 <prestgg12@gmail.com>
2024-12-14 17:07:08 +0400

    Asro Swump Installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
+import swup from '@swup/astro';
 import cloudflare from '@astrojs/cloudflare';
 
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(),swup({
+    preload:{
+      hover: true,
+      visible: false
+    }
+  })],
   adapter: cloudflare()
 });
