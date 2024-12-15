38f50cd7cbdef8cec34400e81b56209c989e32db
Prestgg1 <prestgg12@gmail.com>
2024-12-15 11:42:50 +0400

    SiteMap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import swup from '@swup/astro';
 import cloudflare from '@astrojs/cloudflare';
-
+import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://azescript.pages.dev/',
   integrations: [tailwind(), vue(),swup({
     preload:{
       hover: true,
       visible: false
     }
-  })],
+  }),sitemap()],
   adapter: cloudflare()
 });
--- src/components/HeaderDynamic.vue
+++ src/components/HeaderDynamic.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import Swup from 'swup';
-
 const swup = new Swup();
 const links = [
   { href: '/', text: 'Ana Səhifə' },
@@ -85,7 +84,7 @@ onBeforeUnmount(() => {
     </button>
 
     <!-- Desktop navigation links -->
-    <div class="hidden md:flex space-x-8">
+    <div class="hidden md:flex justify-center items-center space-x-8">
       <span
         v-for="link in links"
         :key="link.href"
@@ -97,6 +96,7 @@ onBeforeUnmount(() => {
       >
         {{ link.text }}
       </span>
+    
     </div>
 
     <!-- Mobile menu (visible only when open) with animation -->
@@ -110,6 +110,7 @@ onBeforeUnmount(() => {
       '-translate-y-10 opacity-0': !isMenuOpen,
     }"
   >
+    
     <span
       v-for="link in links"
       :key="link.href"
